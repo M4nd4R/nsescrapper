9c30b7c4d74ce0f74fa6d2bc887bd012aabf98b7
Mandar Shinde <mandar.shinde2007@gmail.com>
2021-05-16 23:54:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import datetime, os, time
 now = datetime.datetime.now()
 now_time = now.strftime("%H%M")
 
+# Until the time between 09:15 and 15:30, sleep for 5 min
+while int(now_time) < 915 or int(now_time) >= 1530:
+    time.sleep(300)
+
+# If time is between 09:15 and 15:30, start pulling the data every 5 mins
 while int(now_time) >= 915 and int(now_time) <= 1530:
     os.system('python /Users/mandar.shinde/personal/nse/nse_scrapper.py')
     time.sleep(300)
